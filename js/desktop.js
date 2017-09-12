f2e475c5874e669fa5ee63781880abe83a506fd5
unknown <emailvinolia@yahoo.com>
2017-09-12 14:39:40 -0500

    Finalized Code & added README

--- js/desktop.js
+++ js/desktop.js
@@ -4,9 +4,11 @@ $(document).ready (function () {
         $("#main-links").appendTo("header .navbar-header");
     }
     $('section.write-reviews .btn').click(function () {
-    var review = $('section.write-reviews input').val();   
-    $('<div class="review">' + review + '</div>').appendTo('section.reviews');
-    $('section.write-reviews input').val("") 
+    var review = $('section.write-reviews input').val(); 
+    if (review != "") {
+       $('<div class="review">' + review + '</div>').appendTo('section.reviews');
+        $('section.write-reviews input').val("") 
+    }     
   });
 });
 $('.tile')
@@ -20,14 +22,10 @@ $('.tile')
     .on('mousemove', function(e){
       $(this).children('.photo').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
     })
-    // tiles set up
     .each(function(){
       $(this)
         // add a photo container
         .append('<div class="photo"></div>')
-        // some text just to show zoom level on current item in this example
-        //.append('<div class="txt"><div class="x">'+ $(this).attr('data-scale') +'x</div>ZOOM ON<br>HOVER</div>')
-        // set up a background image for each tile based on data-image attribute
         .children('.photo').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
     });
 
